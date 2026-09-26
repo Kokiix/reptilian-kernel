0428a35419849e71f6532bdb813d8961c63a6ab5
kenneth <kokiidoki@gmail.com>
2026-09-26 15:46:28 +0000

    i think using a whole cred for just a kuid was causing segfault?

--- kernel/project_1_messaging.c
+++ kernel/project_1_messaging.c
@@ -7,7 +7,7 @@
 
 struct msg_item {
 	char* msg_contents;
-	struct cred* recipient;
+	kuid_t recipient;
 	struct list_head list_node;
 };
 
@@ -15,26 +15,20 @@ static LIST_HEAD(msg_q_head);
 
 SYSCALL_DEFINE2(send_message_call, char __user *, msg, uid_t, recipient_id)
 {
-	// copy string
+	// copy/allocate string
 	int msg_len = strlen(msg);
 	char* kspace_msg = kmalloc(strnlen_user(msg, PAGE_SIZE), GFP_KERNEL);
 	if (!kspace_msg) {return -ENOMEM;}
-	strncpy_from_user(kspace_msg, msg, msg_len);
+	if (strncpy_from_user(kspace_msg, msg, msg_len) < 0) {return -EFAULT;}
 
-	// copy ID
-	struct cred *kspace_id;
-	kspace_id = prepare_creds();
-	if (!kspace_msg) {return -ENOMEM;}
-	kspace_id->uid = make_kuid(current_user_ns(), recipient_id);
-
-	// create msg struct
+	// allocate msg struct
 	struct msg_item *new_message;
 	new_message = kmalloc(sizeof(*new_message), GFP_KERNEL);
 	if (!new_message) {return -ENOMEM;}
 
 	// fill out msg struct
 	new_message->msg_contents = kspace_msg;
-	new_message->recipient = kspace_id;
+	new_message->recipient = make_kuid(current_user_ns(), recipient_id);
 	INIT_LIST_HEAD(&new_message->list_node);
 	list_add_tail(&new_message->list_node, &msg_q_head);
 
@@ -44,7 +38,7 @@ SYSCALL_DEFINE2(send_message_call, char __user *, msg, uid_t, recipient_id)
     list_for_each_entry(pos, &msg_q_head, list_node) {
         printk(KERN_INFO "  Msg: %s\t For: %d", 
 			pos->msg_contents,
-			pos->recipient->uid);
+			from_kuid(current_user_ns(), pos->recipient));
     }
 
 	return 0;
